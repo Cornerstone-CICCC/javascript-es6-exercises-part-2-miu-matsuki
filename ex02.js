42175b128c1db79a6af13d4d1d77042979fa7be2
miu-matsuki <miu.m.works@gmail.com>
2026-05-27 12:14:44 -0700

    add ex02.js

--- ex02.js
+++ ex02.js
@@ -1,3 +1,5 @@
 // Find Exercise
 // Instructions: Create an array of at least five numbers. Write code to find the first element that is greater than 10.
 
+const numbers = [12,1,5,100,4];
+console.log(numbers.find((num)=> num > 10));
