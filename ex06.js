7e4b1ef1d15562cbb8c74adc02411ccfb7080cef
miu-matsuki <miu.m.works@gmail.com>
2026-05-27 12:27:04 -0700

    add ex06.js

--- ex06.js
+++ ex06.js
@@ -1,3 +1,6 @@
 // Every Exercise
 // Instructions: Create an array of at least five numbers. Write code to check if every number in the array is positive.
 
+const numbers = [1,20,3,54,0];
+
+console.log(numbers.every((num) => num > 0));
