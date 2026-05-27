b080e65cae0a3011ecc199b7f5761549babeb3eb
miu-matsuki <miu.m.works@gmail.com>
2026-05-27 12:24:01 -0700

    add ex05.js

--- ex05.js
+++ ex05.js
@@ -1,3 +1,8 @@
 // Some Exercise
 // Instructions: Create an array of at least five numbers. Write code to check if some of the numbers are even.
 
+
+//const numbers = [1,20,3,54,3];
+const numbers = [1,11,3,11,3];
+
+console.log(numbers.some((num)=> num % 2 === 0));
